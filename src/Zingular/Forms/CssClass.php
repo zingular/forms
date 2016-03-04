05a8a5a6e457fe62bbafb956e0223a547e7d2466
Michiel Leideman <m.leideman@idinteractive.nl>
2016-03-04 13:55:45 +0100

    added css class constant class

---
+++ src/Zingular/Forms/CssClass.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michielleideman
+ * Date: 04-03-16
+ * Time: 13:47
+ */
+
+namespace Zingular\Forms;
+
+/**
+ * Class CssClass
+ * @package Zingular\Forms
+ */
+class CssClass
+{
+    const EXTRA_SMALL = 'size_xs';
+    const SMALL = 'size_s';
+    const MEDIUM = 'size_m';
+    const LARGE = 'size_l';
+    const EXTRA_LARGE = 'size_xl';
+}
