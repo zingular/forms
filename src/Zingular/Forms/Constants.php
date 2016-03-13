710dd88fbfc4baa6e2099c5d123af2c576f8c1cc
Michiel Leideman <m.leideman@gmail.com>
2016-03-13 22:06:13 +0100

    see previous commit

---
+++ src/Zingular/Forms/Constants.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giel
+ * Date: 13-3-2016
+ * Time: 19:28
+ */
+
+namespace Zingular\Forms;
+
+/**
+ * Interface Constants
+ * @package Zingular\Forms
+ */
+interface Constants
+{
+    const POSITION_START = 0;
+    const POSITION_END = -1;
+
+    const VALIDATOR_SERIALIZE = 'serialize';
+}
