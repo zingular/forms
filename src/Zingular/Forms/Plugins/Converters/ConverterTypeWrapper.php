ec00410233075c331483c14f024746d7efcf6c4a
Michiel Leideman <m.leideman@gmail.com>
2016-03-15 22:42:39 +0100

    added classes for plugin type wrappers

---
+++ src/Zingular/Forms/Plugins/Converters/ConverterTypeWrapper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giel
+ * Date: 15-3-2016
+ * Time: 22:41
+ */
+
+namespace Zingular\Forms\Plugins\Converters;
+
+
+class ConverterTypeWrapper
+{
+
+}
---
+++ src/Zingular/Forms/Plugins/Evaluators/FilterTypeWrapper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giel
+ * Date: 15-3-2016
+ * Time: 22:42
+ */
+
+namespace Zingular\Forms\Plugins\Evaluators;
+
+
+class FilterTypeWrapper
+{
+
+}
